46b1ee065d42f585bdece41ef5b0dbb84e9348f6
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-04-13 02:11:56 +0900

    indent change to argument

--- src/targets/swift/urlsession/client.ts
+++ src/targets/swift/urlsession/client.ts
@@ -118,11 +118,11 @@ export const urlsession: Client<UrlsessionOptions> = {
         const value = query[1];
         switch (Object.prototype.toString.call(value)) {
           case '[object String]':
-            push(`${opts.indent}URLQueryItem(name: "${key}", value: "${value}"),`);
+            push(`URLQueryItem(name: "${key}", value: "${value}"),`, 1);
             break;
           case '[object Array]':
             (value as string[]).forEach((val: string) => {
-              push(`${opts.indent}URLQueryItem(name: "${key}", value: "${val}"),`);
+              push(`URLQueryItem(name: "${key}", value: "${val}"),`, 1);
             });
             break;
         }
