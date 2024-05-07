38752c5e7a44f37e68ef42248e312e32bffe1d58
Darren Yong <contact.darrenyong@gmail.com>
2024-05-07 12:12:07 -0700

    refactor: remove node `request` target

--- src/targets/node/target.ts
+++ src/targets/node/target.ts
@@ -3,7 +3,6 @@ import type { Target } from '../index.js';
 import { axios } from './axios/client.js';
 import { fetch } from './fetch/client.js';
 import { native } from './native/client.js';
-import { request } from './request/client.js';
 import { unirest } from './unirest/client.js';
 
 export const node: Target = {
@@ -15,7 +14,6 @@ export const node: Target = {
   },
   clientsById: {
     native,
-    request,
     unirest,
     axios,
     fetch,
