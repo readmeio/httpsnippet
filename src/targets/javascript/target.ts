fea4e186358b3e78411d79ab7857e038386a19c8
Darren Yong <contact.darrenyong@gmail.com>
2024-05-07 12:11:11 -0700

    refactor: remove xhr target

--- src/targets/javascript/target.ts
+++ src/targets/javascript/target.ts
@@ -3,17 +3,14 @@ import type { Target } from '../index.js';
 import { axios } from './axios/client.js';
 import { fetch } from './fetch/client.js';
 import { jquery } from './jquery/client.js';
-import { xhr } from './xhr/client.js';
 
 export const javascript: Target = {
   info: {
     key: 'javascript',
     title: 'JavaScript',
-    default: 'xhr',
   },
 
   clientsById: {
-    xhr,
     axios,
     fetch,
     jquery,
