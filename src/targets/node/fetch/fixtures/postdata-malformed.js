e6ac3a0d5fd5a43ae9cfef6fc6c64be681508847
Darren Yong <contact.darrenyong@gmail.com>
2024-05-29 11:12:09 -0700

    refactor: oops

--- src/targets/node/fetch/fixtures/postdata-malformed.js
+++
@@ -1,6 +0,0 @@
-const options = {method: 'POST', headers: {'content-type': 'application/json'}};
-
-fetch('https://httpbin.org/anything', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
--- src/targets/node/target.ts
+++ src/targets/node/target.ts
@@ -1,18 +1,16 @@
 import type { Target } from '../index.js';
 
 import { axios } from './axios/client.js';
-import { fetch } from './fetch/client.js';
 import { unirest } from './unirest/client.js';
 
 export const node: Target = {
   info: {
     key: 'node',
     title: 'Node.js',
-    default: 'fetch',
+    default: 'axios',
     cli: 'node %s',
   },
   clientsById: {
-    fetch,
     unirest,
     axios,
   },
