bf7badc8f0a1fa604170be54542f93a042ae395e
Darren Yong <contact.darrenyong@gmail.com>
2024-05-29 10:33:09 -0700

    feat: replace node-fetch with fetch

--- src/targets/node/target.ts
+++ src/targets/node/target.ts
@@ -1,16 +1,18 @@
 import type { Target } from '../index.js';
 
 import { axios } from './axios/client.js';
+import { fetch } from './fetch/client.js';
 import { unirest } from './unirest/client.js';
 
 export const node: Target = {
   info: {
     key: 'node',
     title: 'Node.js',
-    default: 'axios',
+    default: 'fetch',
     cli: 'node %s',
   },
   clientsById: {
+    fetch,
     unirest,
     axios,
   },
