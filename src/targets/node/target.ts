ac35e4b670436635a0053a05ddd2d76a1c9f1c06
Darren Yong <contact.darrenyong@gmail.com>
2024-06-03 12:45:23 -0700

    refactor: add native `fetch` to node

--- src/targets/node/target.ts
+++ src/targets/node/target.ts
@@ -1,17 +1,19 @@
 import type { Target } from '../index.js';
 
 import { axios } from './axios/client.js';
+import { fetch } from './fetch/client.js';
 import { unirest } from './unirest/client.js';
 
 export const node: Target = {
   info: {
     key: 'node',
     title: 'Node.js',
-    default: 'axios',
+    default: 'fetch',
     cli: 'node %s',
   },
   clientsById: {
     unirest,
     axios,
+    fetch,
   },
 };
