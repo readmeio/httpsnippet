e31c0539700f26e9edf788978ce034eeebc0f7c5
Jon Ursenbach <jon@ursenba.ch>
2024-09-05 10:32:02 -0700

    Revert "refactor: removing js `xhr`"
    This reverts commit fd170ff4e8daa941046c653c7cb8bb592c8d5aea.

--- src/targets/javascript/target.ts
+++ src/targets/javascript/target.ts
@@ -3,15 +3,17 @@ import type { Target } from '../index.js';
 import { axios } from './axios/client.js';
 import { fetch } from './fetch/client.js';
 import { jquery } from './jquery/client.js';
+import { xhr } from './xhr/client.js';
 
 export const javascript: Target = {
   info: {
     key: 'javascript',
     title: 'JavaScript',
-    default: 'fetch',
+    default: 'xhr',
   },
 
   clientsById: {
+    xhr,
     axios,
     fetch,
     jquery,
