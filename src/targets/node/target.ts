f52782e3a7f8661ad3bd0cecb9772ef19e2bd210
Jon Ursenbach <jon@ursenba.ch>
2024-09-04 17:05:27 -0700

    refactor: removing node `native`

--- src/targets/node/target.ts
+++ src/targets/node/target.ts
@@ -2,17 +2,15 @@ import type { Target } from '../index.js';
 
 import { axios } from './axios/client.js';
 import { fetch } from './fetch/client.js';
-import { native } from './native/client.js';
 
 export const node: Target = {
   info: {
     key: 'node',
     title: 'Node.js',
-    default: 'native',
+    default: 'fetch',
     cli: 'node %s',
   },
   clientsById: {
-    native,
     axios,
     fetch,
   },
